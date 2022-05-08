34f036b3e9b3f16fcc95b85c347715762a4a22ee
Pedro Chang <pedrochang91@gmail.com>
2022-05-07 22:35:26 -0700

    adds arrays_map_practice problems.rb

--- hash_practice_problems.rb
+++ hash_practice_problems.rb
@@ -1,6 +1,17 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+person = { "first_name" => "Pedro", "last_name" => "Chang", "email" => "pedro.chang@gmail.com" }
+p person["first_name"]
+p person["last_name"]
+p person["email"]
 
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+people = [
+  { "first_name" => "Jeff", "last_name" => "Hopper" },
+  { "first_name" => "Alice", "last_name" => "Walker" },
+  { "first_name" => "James", "last_name" => "Buchanan" },
+]
+p people[0]["first_name"]
+p people[0]["last_name"]
 
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
